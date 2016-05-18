f7a49cbffacb248b1ad8ee4d1d8c46507120b99c
Zulfiya <yusupova.zulfiya@gmail.com>
2016-05-19 01:54:44 +0600

    trying to parse json from file

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,6 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+var url = "http://127.0.0.1:8000/api/words/en/?format=json"
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
+var data={"users":[
+        {
+            "firstName":"Ray",
+            "lastName":"Villalobos",
+            "joined":2012
+        },
+        {
+            "firstName":"John",
+            "lastName":"Jones",
+            "joined":2010
+        }
+]}
+document.getElementById("placeholder").innerHTML=data.users[0].firstName + " " + data.users[0].lastName+" "+ data.users[0].joined;
