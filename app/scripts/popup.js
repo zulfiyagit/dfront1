a40988ab797b107647e8167784a746e39ea81144
Zulfiya <yusupova.zulfiya@gmail.com>
2016-05-19 05:30:33 +0600

    commit

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,4 +1,20 @@
-var url = "http://127.0.0.1:8000/api/words/en/?format=json"
- /
-var data=JSON.parse(url);
-document.getElementById("placeholder").innerHTML=data.title+ " " + data.definition;
+/*$.ajax({
+        url: siteUrl + "/api/words/en/?format=api",
+        method: "GET",
+        headers: { "Content- Type": "application/json; "},
+        success: function (data) {
+           console.log(JSON.stringify(data.d.results));
+        },
+        error: function (data) {
+           console.log(JSON.stringify(data));
+        }
+});*/
+
+var text = '{"words":[' +
+'{"title":"Apple","definition":"is a fruit" , "translation":[{"title":"alma","definition":"zhemis"}, {"title":"yablokoo","definition":"frukt "}]},' +
+'{"title":"yabloko","definition":"Smith" },' +
+'{"title":"Peter","definition":"Jones" }]}';
+
+obj = JSON.parse(text);
+document.getElementById("placeholder").innerHTML =
+obj.words[0].title+ " " + obj.words[0].translation[1].title;
