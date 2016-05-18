de5a17ea6b17637994714788eada790a364e9e59
Zulfiya <yusupova.zulfiya@gmail.com>
2016-05-19 04:11:36 +0600

    third commit

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,15 +1,4 @@
 var url = "http://127.0.0.1:8000/api/words/en/?format=json"
-
-var data={"users":[
-        {
-            "firstName":"Ray",
-            "lastName":"Villalobos",
-            "joined":2012
-        },
-        {
-            "firstName":"John",
-            "lastName":"Jones",
-            "joined":2010
-        }
-]}
-document.getElementById("placeholder").innerHTML=data.users[0].firstName + " " + data.users[0].lastName+" "+ data.users[0].joined;
+ /
+var data=JSON.parse(url);
+document.getElementById("placeholder").innerHTML=data.title+ " " + data.definition;
